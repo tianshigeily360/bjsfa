798c273b57bcfbb654be2a3243549f1acc340aae
zyr <3254798598@qq.com>
2019-06-18 20:44:04 +0800

    zrmaj

--- src/views/AddShop.vue
+++ src/views/AddShop.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="addshop">
-
+    <TopHeader title="新增门店">
+      <div slot="r"></div>
+    </TopHeader>
+    <div class="shopmap">
+      <img src="../assets/map.png" alt="">
+    </div>
   </div>
 </template>
 
 <script>
+import TopHeader from '../components/TopHeader';
 export default {
-  name: 'addshop',
+  name: 'AddShop',
   data() {
     return {
 
     };
+  },
+  components: {
+    TopHeader
   }
 };
 </script>
