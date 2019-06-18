cb5b0512a9c0f712eb2ad4924243b1308f67aaea
zyr <3254798598@qq.com>
2019-06-18 19:51:22 +0800

    zyr cm

---
+++ src/views/AddShop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="addshop">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addshop',
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
