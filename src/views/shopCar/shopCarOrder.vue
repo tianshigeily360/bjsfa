b0af4e4701767bd89b429f17566976ed5e8bc1ee
jmaking <tianshigeily360@gmail.com>
2019-06-19 15:27:59 +0800

    category again

---
+++ src/views/shopCar/shopCarOrder.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="order"></div>
+</template>
+
+<script>
+export default {
+  name: "order",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
