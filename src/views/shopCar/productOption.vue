9aced2956553fd2b1ef29e64b8e846e012fa820c
wyd <614353869@qq.com>
2019-06-22 16:57:50 +0800

    added shopcar

--- src/views/shopCar/productOption.vue
+++ src/views/shopCar/productOption.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="menu-top">
       <div class="menuLeft">
-        <img :src="proData.img" alt />
+        <img :src="proData.img" alt>
       </div>
       <div class="menuRight">
         <p class="first">{{ proData.title }}</p>
@@ -58,7 +58,8 @@ export default {
     return {
       proData: "",
       numData: 1,
-      actName:""
+      actName: "",
+      money: ""
     };
   },
   components: {
@@ -81,9 +82,14 @@ export default {
     },
     //跳转到购物车
     goShopCar() {
+      //计算加入购物车的商品总价
+      this.money = this.proData.monery * this.numData;
+
       this.$store.commit("initshopCarData", {
         item: this.proData,
-        num: this.numData
+        num: this.numData,
+        totalprice: this.money,
+        isMarked: true
       });
       this.$router.go(-1);
     }
